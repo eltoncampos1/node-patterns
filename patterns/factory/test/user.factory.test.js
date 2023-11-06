3e4be156325cbbf4a3fb8642bacdee5fd5ac03a5
Elton Campos <eltoncampos36@gmail.com>
2023-11-06 16:46:48 -0300

    [test]: user factory

---
+++ patterns/factory/test/user.factory.test.js
@@ -0,0 +1,29 @@
+const rewiremock = require("rewiremock/node")
+const { deepStrictEqual } = require("assert")
+const dbData = [ {name: "Jhon doe"}, {name: "FooBar"}]
+
+class MockDatabase {
+    connect = () => this
+    find = async (query) => dbData
+}
+
+rewiremock(() => require("../src/util/database.js")).with(MockDatabase)
+;(async () => {
+    {
+        const expected = [{name: "JHON DOE"}, {name: "FOOBAR"}]
+        rewiremock.enable()
+        const UserFactory = require("../src/factory/user.factory.js")
+        const userFactory = await UserFactory.createInstance()
+        const result = await userFactory.find("")
+        deepStrictEqual(result, expected)
+        rewiremock.disable()
+    }
+
+    {
+        const expected = [{name: "ELTONCAMPOS1"}]
+        const UserFactory = require("../src/factory/user.factory.js")
+        const userFactory = await UserFactory.createInstance()
+        const result = await userFactory.find("")
+        deepStrictEqual(result, expected)
+    }
+})()
