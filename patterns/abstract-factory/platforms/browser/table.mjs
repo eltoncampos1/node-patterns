8b2bb638c0646d2aa32f4afceb001ee77634f9b7
Elton Campos <eltoncampos36@gmail.com>
2023-11-07 12:01:36 -0300

    [feat]: create browser UI

--- patterns/abstract-factory/platforms/browser/table.mjs
+++ patterns/abstract-factory/platforms/browser/table.mjs
@@ -2,6 +2,32 @@ import TableComponent from "../../shared/base/table.component.mjs";
 
 export default class TableBrowserComponent extends TableComponent {
     render(data) {
-        window.alert("DATA!")
+        const template = this.prepareData(data)
+        document.body.insertAdjacentHTML("afterBegin", template)
     }
+    prepareData(data) {
+        const [firstItem] = data
+        const theaders = Object.keys(firstItem)
+                        .map(text => `<th scope="col">${text}</th>`)
+
+        const joinLists = list => list.join("")
+
+        const tbodyValues = data
+                                .map(item => Object.values(item))
+                                .map(item => item.map(value => `<td>${value}</td>`))
+                                .map(tds => `<tr>${joinLists(tds)}</tr>`)
+
+        const template = `
+            <table class="table">
+                <thread>
+                    <tr>${joinLists(theaders)}</tr>
+                </thread>
+                <tbbody>
+                    ${joinLists(tbodyValues)}
+                </tbbody>
+            </table>
+        `
+        return template
+    }
+
 }
